7f23c77e96f7e101eb813f073020bd1bd3b889c4
bright <lious68@163.com>
2019-08-12 21:46:35 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-public_key  = ""
-private_key = ""
-project_id = "org-xxx" # 项目ID 请在Dashbord 上获取
-region = "cn-bj2"
-
-renew = "NO" #程序是否真实续费，或者只打印eip关联资源。“YES”为开启续费，"NO"则不续费。请注意，开启续费（YES）会真实对资源续费操作。
